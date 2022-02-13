78adea7209442d04a570cbd9d296897fa59dc7ef
aidid-nibir <aidid.nibir97@gmail.com>
2022-02-13 13:42:12 +0600

    edited reducers in a more structurized way

--- src/Store/reducers/counterReducer.js
+++ src/Store/reducers/counterReducer.js
@@ -1,4 +1,5 @@
 import * as actionsTypes from '../Actions/actionTypes'
+import { updateObject } from '../Utility'
 
 const initialState = {
     ouch: 0,
@@ -7,26 +8,61 @@ const initialState = {
 const counterReducer = (state = initialState, action) => {
     switch (action.type) {
         case actionsTypes.INCREMENT:
-            const newState = Object.assign({}, state);
-            newState.ouch = state.ouch + 1
-            return newState
+            return updateObject(state, { ouch: state.ouch + 1 });
         case actionsTypes.DECREMENT:
-            return {
-                ...state,
-                ouch: state.ouch - 1
-            }
+            return updateObject(state, { ouch: state.ouch - 1 });
         case actionsTypes.ADD:
-            return {
-                ...state,
-                ouch: state.ouch + action.val
-            }
+            return updateObject(state, { ouch: state.ouch + action.val });
         case actionsTypes.SUBTRACT:
-            return {
-                ...state,
-                ouch: state.ouch - action.val
-            }
+            return updateObject(state, { ouch: state.ouch - action.val });
         default:
     }
     return state;
 }
 export default counterReducer;
+
+// plese find the old approach bellow
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// =========== old approach to update the states bellow ===========
+// ================================================================
+// const counterReducer = (state = initialState, action) => {
+//     switch (action.type) {
+//         case actionsTypes.INCREMENT:
+//             const newState = Object.assign({}, state);
+//             newState.ouch = state.ouch + 1
+//             return newState
+//         case actionsTypes.DECREMENT:
+//             return {
+//                 ...state,
+//                 ouch: state.ouch - 1
+//             }
+//         case actionsTypes.ADD:
+//             return {
+//                 ...state,
+//                 ouch: state.ouch + action.val
+//             }
+//         case actionsTypes.SUBTRACT:
+//             return {
+//                 ...state,
+//                 ouch: state.ouch - action.val
+//             }
+//         default:
+//     }
+//     return state;
+// }
+// ================================================================
--- src/Store/reducers/resultReducer.js
+++ src/Store/reducers/resultReducer.js
@@ -1,28 +1,65 @@
 import * as actionsTypes from '../Actions/actionTypes'
+import { updateObject } from '../Utility'
 
 const initialState = {
     results: []
 }
 
+const deleteResult = (state, action) => {
+    const updatedArray = state.results.filter((el, index) => el.id !== action.selectedElementID)
+    return updateObject(state, { results: updatedArray });
+}
+
 const resultReducer = (state = initialState, action) => {
     switch (action.type) {
         case actionsTypes.STORE_RESULT:
-            return {
-                ...state,
-                results: state.results.concat({ id: new Date(), Value: action.result })
-            }
+            return updateObject(state, { results: state.results.concat({ id: new Date(), Value: action.result }) });
         case actionsTypes.DELETE_RESULT:
-            // const id = 1;
-            // const newArray = [...state.results];
-            // newArray.splice(id, 1);
-            const updatedArray = state.results.filter((el, index) => el.id !== action.selectedElementID)
-            return {
-                ...state,
-                results: updatedArray
-            }
+            return deleteResult(state, action);
         default:
 
     }
     return state;
 }
 export default resultReducer;
+// plese find the old approach bellow
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// =========== old approach to update the states bellow ===========
+// ================================================================
+// const resultReducer = (state = initialState, action) => {
+//     switch (action.type) {
+//         case actionsTypes.STORE_RESULT:
+//             return {
+//                 ...state,
+//                 results: state.results.concat({ id: new Date(), Value: action.result })
+//             }
+//         case actionsTypes.DELETE_RESULT:
+//             // const id = 1;
+//             // const newArray = [...state.results];
+//             // newArray.splice(id, 1);
+//             const updatedArray = state.results.filter((el, index) => el.id !== action.selectedElementID)
+//             return {
+//                 ...state,
+//                 results: updatedArray
+//             }
+//         default:
+
+//     }
+//     return state;
+// }
+// ================================================================
